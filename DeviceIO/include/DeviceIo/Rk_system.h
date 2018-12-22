d9978e78fc91c2a07bc5d3c6f51dd7549aa3a299
Yu YongZhen <yuyz@rock-chips.com>
2018-12-22 22:09:41 +0800

    Release Version V1.0.0
    Change-Id: I2550073c43c517e3f7ed2e45ed5769cb9c7d22a5
    Signed-off-by: Yu YongZhen <yuyz@rock-chips.com>

--- DeviceIO/include/DeviceIo/Rk_system.h
+++ DeviceIO/include/DeviceIo/Rk_system.h
@@ -5,8 +5,14 @@
 extern "C" {
 #endif
 
+/*
+ *Version 1.0 Release 2018/12/22
+ */
+
+#define DEVICEIO_VERSION "V1.0.0"
 
 int RK_read_chip_id(char *buffer, const int size);
+int RK_read_version(char *buffer, const int size);
 
 
 #ifdef __cplusplus
