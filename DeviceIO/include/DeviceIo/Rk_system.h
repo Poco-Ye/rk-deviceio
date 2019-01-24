25d105577b272118ada82fa435be68af2c89371d
Yu YongZhen <yuyz@rock-chips.com>
2019-01-24 11:28:58 +0800

    Release version V1.0.2
    1.ble_wifi
      1. support wep key
      2. optimize wpa_supplicant.conf
      3. fix many times reports sink connected
      4. add ble_config wifi timeout value
      5. fix chinese code for hisense
    2.Rk_key: add fix register long press callback with different keyevent
    
    Change-Id: I3a0381d1c816f3f5b15c62cc5c61024d3b0e3a09
    Signed-off-by: Yu YongZhen <yuyz@rock-chips.com>

--- DeviceIO/include/DeviceIo/Rk_system.h
+++ DeviceIO/include/DeviceIo/Rk_system.h
@@ -6,6 +6,15 @@ extern "C" {
 #endif
 
 /*
+ *Version 1.0.2 Release 2019/01/24
+  1.ble_wifi
+    1. support wep key
+    2. optimize wpa_supplicant.conf
+    3. fix many times reports sink connected
+    4. add ble_config wifi timeout value
+    5. fix chinese code for hisense
+  2.Rk_key: add fix register long press callback with different keyevent
+
  *Version 1.0.1 Release 2019/01/07
   1.ble_wifi:	fix Chinese coding problem
 		fix ble report event and add wifi priority
@@ -18,7 +27,7 @@ extern "C" {
  *Version 1.0.0 Release 2018/12/22
  */
 
-#define DEVICEIO_VERSION "V1.0.1"
+#define DEVICEIO_VERSION "V1.0.2"
 
 int RK_read_chip_id(char *buffer, const int size);
 int RK_read_version(char *buffer, const int size);
