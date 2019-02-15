eaf103c5680360c1691f3dc4874516cf8ce7c2b1
jorge.ge <jorge.ge@rock-chips.com>
2019-02-15 15:37:13 +0800

    deviceio: suport log save local file
    Change-Id: I259f9353e3938b8227013c2efb58d3c1b1d7afb3
    Signed-off-by: jorge.ge <jorge.ge@rock-chips.com>

--- DeviceIO/include/DeviceIo/RK_log.h
+++ DeviceIO/include/DeviceIo/RK_log.h
@@ -5,6 +5,11 @@
 extern "C" {
 #endif
 
+const int RK_LOG_TYPE_CONSOLE = 0x01;
+const int RK_LOG_TYPE_FILE = 0x02;
+
+int RK_LOG_set_type(const int type);
+int RK_LOG_set_save_parameter(const char saveLevel, const char *dir, const int fileSize, const int fileNu);
 int RK_LOGV(const char *format, ...);
 int RK_LOGI(const char *format, ...);
 int RK_LOGE(const char *format, ...);
