9f3cd59dd1484528d4d2b97315664f0996f2976c
jorge.ge <jorge.ge@rock-chips.com>
2019-04-18 09:15:42 +0800

    fixup2! deviceio: fix effect of type NONE can't timeout
    Change-Id: If99e2ccc23d7ba7f14cb2d7d1a983856045f6eb4
    Signed-off-by: jorge.ge <jorge.ge@rock-chips.com>

--- DeviceIO/src/linux/Rk_led.cpp
+++ DeviceIO/src/linux/Rk_led.cpp
@@ -139,7 +139,7 @@ static int led_wait_new_command(void)
 			if (effect) {
 				if (effect->effect->type == Led_Effect_type_NONE) {
 					if (effect->effect->timeout <= 0) {
-						pthread_cond_wait(&m_led_manager.cond, &m_led_manager.mutex, &tout);
+						pthread_cond_wait(&m_led_manager.cond, &m_led_manager.mutex);
 					} else {
 						tout.tv_nsec += 1000000 * effect->effect->period;
 						while (tout.tv_nsec > 1000000000) {
